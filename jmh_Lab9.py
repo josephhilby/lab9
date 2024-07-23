ce6783da337008193438c9efe735fe8357c18aea
Joseph Hilby <joseph.hilby@gmail.com>
2024-07-23 12:45:19 -0500

    create simple ui

--- jmh_Lab9.py
+++ jmh_Lab9.py
@@ -7,8 +7,34 @@ def encode(password):
     return encoded_password
 
 
+def display_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print("")
+
+
 def main():
-    print(encode("00009962"))
+    logged_in = True
+    current_password = None
+
+    while logged_in:
+        display_menu()
+        user_input = input("Please enter an option: ")
+
+        match user_input:
+            case "1":
+                user_input = input("Please enter the password to encode: ")
+                current_password = encode(user_input)
+                if current_password:
+                    print("Your password has been encoded and stored!")
+                else:
+                    print("Your password was not stored!")
+            case "3":
+                logged_in = False
+        print("")
 
 
 if __name__ == "__main__":
